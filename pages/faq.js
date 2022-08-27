edbec6eab756df7b6c2d87ba6527565623bd4f14
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-08-27 11:03:49 -0300

    link component

---
+++ pages/faq.js
@@ -0,0 +1,12 @@
+import Link from '../src/components/Link';
+
+function faq() {
+  return (
+    <div>
+      <h1>FAQ</h1>
+      <Link href='/'>Ir para o home</Link>
+    </div>
+  );
+}
+
+export default faq;
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Link from '../src/components/Link';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Home Page - Alura Cases</h1>
+      <Link href='/faq'>FAQ</Link>
+    </div>
+  );
+}
+
+export default HomePage;
