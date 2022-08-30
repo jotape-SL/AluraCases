2b768516421a25c800d6c2fa4a7b906531fb1121
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-08-29 22:10:40 -0300

    getStaticProps

--- pages/faq.js
+++ pages/faq.js
@@ -1,20 +1,24 @@
 import Link from '../src/components/Link';
 import { useEffect, useState } from 'react';
 
-function FAQPage() {
-  const [faq, setFaq] = useState([]);
-  useEffect(() => {
-    const FAQ_API_URL =
-      'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
-    fetch(FAQ_API_URL)
-      .then((respostaDoServidor) => {
-        return respostaDoServidor.json();
-      })
-      .then((resposta) => {
-        setFaq(resposta);
-      });
-  }, []);
+export async function getStaticProps() {
+  const FAQ_API_URL =
+    'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
+  const faq = await fetch(FAQ_API_URL)
+    .then((respostaDoServidor) => {
+      return respostaDoServidor.json();
+    })
+    .then((resposta) => {
+      return resposta;
+    });
+  return {
+    props: {
+      faq,
+    },
+  };
+}
 
+function FAQPage({ faq }) {
   return (
     <div>
       <h1>FAQ</h1>
