b6755fa1e4c67d948f74e0a3e9d1129f0c4212fe
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-08-29 22:01:22 -0300

    getting data from api and displaying it

--- pages/faq.js
+++ pages/faq.js
@@ -1,12 +1,36 @@
 import Link from '../src/components/Link';
+import { useEffect, useState } from 'react';
 
-function faq() {
+function FAQPage() {
+  const [faq, setFaq] = useState([]);
+  useEffect(() => {
+    const FAQ_API_URL =
+      'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
+    fetch(FAQ_API_URL)
+      .then((respostaDoServidor) => {
+        return respostaDoServidor.json();
+      })
+      .then((resposta) => {
+        setFaq(resposta);
+      });
+  }, []);
+
   return (
     <div>
       <h1>FAQ</h1>
       <Link href='/'>Ir para o home</Link>
+      <ul>
+        {faq.map(({ answer, question }) => {
+          return (
+            <li key={question}>
+              <h2>{question}</h2>
+              <p>{answer}</p>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
 
-export default faq;
+export default FAQPage;
--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,3 @@
 import HomeScreen from '../src/screens/HomeScreen';
 
 export default HomeScreen;
-
-// import Link from '../src/components/Link';
-// import Footer from '../src/components/patterns/Footer';
-
-// function Title({ children }) {
-//   return (
-//     <>
-//       <h1>{children}</h1>
-//       <style jsx>{`
-//         h1 {
-//           color: red;
-//         }
-//       `}</style>
-//     </>
-//   );
-// }
-
-// function HomePage() {
-//   return (
-//     <div>
-//       <Title>Home Page - Alura Cases</Title>
-//       <Link href='/faq'>FAQ</Link>
-//       <Footer />
-//     </div>
-//   );
-// }
-
-// export default HomePage;
